89e4240f75db9842ec1bdd107b3d9b39956fb90c
idsbllp <idsbllp@gmail.com>
2017-08-22 09:53:07 +0800

    fix:url & loading

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -2,7 +2,7 @@
     <div class="index">
         <input type="text" class="username" v-model="username" placeholder="请输入姓名">
         <input type="text" class="admission" v-model="admission" placeholder="请输入准考证号">
-        <input type="button" class="submit" @click="submit" value="提交">
+        <input type="button" class="submit" @click="submit" v-model="sub">
     </div>
 </template>
 
@@ -12,21 +12,25 @@
         data() {
             return {
                 username: '',
-                admission: ''
+                admission: '',
+                sub: '提交',
+                loading: false
             }
         },
         methods: {
-            submit() {
+            submit(e) {
                 // const urlPrefix = 'http://hongyan.cqupt.edu.cn/llp.php?'
-                const urlPrefix = 'http://jx3536.s1.natapp.link/index.php?'
+                // const urlPrefix = 'http://jx3536.s1.natapp.link/index.php?'
                 // const urlPrefix = 'http://115.28.50.25/index.php?'
                 // const urlPrefix = 'http://jx3536.s1.natapp.link/index.php?'
+                const urlPrefix = 'http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/InquiryExam/InquiryExam/getCETScore&'
                 if (!this.username) {
                     return swlt('请输入姓名', '', 'error')
                 }
                 if (!this.admission || !Number(this.admission)) {
                     return swlt('准考证输入有误', '', 'error')
                 }
+                this.sub = '查询中...'
                 this.$http.get(`${urlPrefix}xm=${encodeURI(this.username)}&zkzh=${this.admission}`).then(res => {
                     res = res.body
                     if (res.Status !== 200) {
@@ -42,9 +46,75 @@
 
 <style lang="less">
     .index {
+        position: relative;
         margin-top: .2rem;
         font-size: 0.4rem;
         overflow: hidden;
+    }
+    .loader {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        img {
+            animation: loading 5s infinite
+        }
+    }
+    @keyframes loading {
+        from {
+            transform: rotate(0);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+    .loading {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: 34px;
+        margin-left: 25px;
+    }
+    
+    .loading-bar {
+        display: inline-block;
+        width: 4px;
+        height: 18px;
+        border-radius: 4px;
+        animation: loading 1s ease-in-out infinite;
+    }
+    
+    .loading-bar:nth-child(1) {
+        background-color: #3498db;
+        animation-delay: 0;
+    }
+    
+    .loading-bar:nth-child(2) {
+        background-color: #c0392b;
+        animation-delay: 0.09s;
+    }
+    
+    .loading-bar:nth-child(3) {
+        background-color: #f1c40f;
+        animation-delay: .18s;
+    }
+    
+    .loading-bar:nth-child(4) {
+        background-color: #27ae60;
+        animation-delay: .27s;
+    }
+    
+    @keyframes loading {
+        0% {
+            transform: scale(1);
+        }
+        20% {
+            transform: scale(1, 1.3);
+        }
+        40% {
+            transform: scale(1);
+        }
     }
     input {
         padding: .4rem;
